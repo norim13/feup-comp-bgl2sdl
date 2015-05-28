889d2276684a2b5d40dc670e9ed1eeb40fd87205
Mira <fil.fmiranda@gmail.com>
2015-05-28 10:59:38 +0000

    Tower

--- Classes/Tower.java
+++ Classes/Tower.java
@@ -1,5 +1,29 @@
 package classes;
 
 public class Tower {
+
+	public String id,designation,radius,height;
+
+	public Coordinate coordinate;
+
+	public Tower() {
+		id="u01";
+		designation="Tower";
+		radius="2.55";
+		height="129";
+		coordinate= new Coordinate();
+	}
+
+	public String toSDL(){
+		String ret = 
+		"<tower id="+id+">"+
+			"<designation>"+designation+"</designation>"+
+			coordinate.toSDL
+			+"<radius lengthUnit=\"Meter\">"radius"</radius>"+
+			"<height lengthUnit=\"Meter\">"height"</height>";
+		"</tower>";
+		
+		return ret;
+	}
 	
 }
