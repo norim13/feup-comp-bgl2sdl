d7155ff35dce8a1997a58e79e02deaccb53adec2
ei12022 <ei12022@8cb38d4a-1028-4903-97e6-7bdece20f59d>
2015-05-28 10:32:45 +0000

    sdl + contact person

---
+++ Classes/ContactPerson.java
@@ -0,0 +1,46 @@
+package classes;
+
+public class ContactPerson {
+
+	public String name, title, institution, position,
+		address, zipCode, city, stateDistrictRegion,country,
+		telephone, cellphone, fax, email;
+	
+	
+	public ContactPerson() {
+		name = "XXX Name XXX";
+		title = "XXX Title XXX";
+		institution = "XXX Institution XXX";
+		position = "XXX Position XXX";
+		address = "XXX Address XXX";
+		zipCode = "XXX ZipCode XXX";
+		city = "XXX City XXX";
+		stateDistrictRegion = "XXX State XXX";
+		country = "XXX Country XXX";
+		telephone = "XXX Telephone XXX";
+		cellphone = "XXX Cellphone XXX";
+		fax = "XXX Fax XXX";
+		email = "XXX email XXX";
+	}
+	
+	public String toSDL(){
+		String ret = 
+		"<contactPerson>"+
+			"<name>"+name+"</name>"+
+			"<title>"+title+"</title>"+
+			"<institution>"+institution+"</institution>"+
+			"<position>"+position+"</position>"+
+			"<address>"+address+"</address>"+
+			"<zipCode>"+zipCode+"</zipCode>"+
+			"<city>"+city+"</city>"+
+			"<stateDistrictRegion>"+stateDistrictRegion+"</stateDistrictRegion>"+
+			"<country>"+country+"</country>"+
+			"<telephone>"+telephone+"</telephone>"+
+			"<cellphone>"+cellphone+"</cellphone>"+
+			"<fax>"+fax+"</fax>"+
+			"<email>"+email+"</email>"+
+		"</contactPerson>";
+		
+		return null;
+	}
+}
