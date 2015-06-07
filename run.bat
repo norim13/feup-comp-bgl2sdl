a1658cc21b7b1e3cd836ae65dcf6c40f46c728b9
João Norim Bandeira <ei12022@fe.up.pt>
2015-06-07 13:31:23 +0000

    change in file structure

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-antlr4 XMLLexer.g4 && antlr4 XMLParser.g4 && javac *.java && java TestXML
+if not exist ".\bin" ( mkdir ".\bin" ) && antlr4 -o bin -package g4 g4\XMLLexer.g4 && antlr4 -o bin -package g4 -lib bin\g4 g4\XMLParser.g4 &&  javac -d bin -cp "lib\antlr-4.5-complete.jar;bin" src\classes\*.java && javac -d bin -cp "lib\antlr-4.5-complete.jar;bin" src\*.java &&  java -cp "bin\.;lib\antlr-4.5-complete.jar" TestXML "xmls\sa-carneiro.xml"
